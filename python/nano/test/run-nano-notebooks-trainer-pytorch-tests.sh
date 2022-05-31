474e521daaa9b17bfb9f02dcf7b9f58690cb4c8e
Mingzhi Hu <49382651+y199387@users.noreply.github.com>
2022-05-31 08:59:37 +0800

    Update notebooks unit test

--- python/nano/test/run-nano-notebooks-trainer-pytorch-tests.sh
+++ python/nano/test/run-nano-notebooks-trainer-pytorch-tests.sh
@@ -4,7 +4,6 @@ export ANALYTICS_ZOO_ROOT=${ANALYTICS_ZOO_ROOT}
 export NANO_HOME=${ANALYTICS_ZOO_ROOT}/python/nano/src
 export PYTORCH_NANO_NOTEBOOKS_DIR=${ANALYTICS_ZOO_ROOT}/python/nano/notebooks/pytorch
 export MAX_STEPS=10
-export MULTIPLE_MAX_STEPS=760
 
 wget -nv ${FTP_URI}/analytics-zoo-data/cifar-10-python.tar.gz -P ${PYTORCH_NANO_NOTEBOOKS_DIR}/cifar10/
 
@@ -13,10 +12,22 @@ set -e
 echo "# Start Testing Train Notebooks"
 start=$(date "+%s")
 
-python -m pytest --nbmake --nbmake-timeout=1000 --nbmake-kernel=python3 ${PYTORCH_NANO_NOTEBOOKS_DIR}/cifar10/nano-trainer-example.ipynb
+python -m pytest --nbmake --nbmake-timeout=1000 --nbmake-kernel=python3 ${PYTORCH_NANO_NOTEBOOKS_DIR}/ -k "train"
 
 now=$(date "+%s")
 time=$((now-start))
 
 echo "BigDL-Nano Train Notebooks tests finished"
 echo "Time used:$time seconds"
+
+echo "# Start Testing Inference Notebooks"
+start=$(date "+%s")
+
+python -m pytest --nbmake --nbmake-timeout=1000 --nbmake-kernel=python3 ${PYTORCH_NANO_NOTEBOOKS_DIR}/ -k "inference"
+
+now=$(date "+%s")
+time=$((now-start))
+
+echo "BigDL-Nano Inference Notebooks tests finished"
+echo "Time used:$time seconds"
+
